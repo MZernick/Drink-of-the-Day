953ac7b292bb4633d0a7ca4d0d7fb74d50fca406
Moonbeam <mfzernick@gmail.com>
2023-01-12 13:05:44 -0600

    psuedcode for js

---
+++ assets/JS/script.js
@@ -0,0 +1,33 @@
+//declare variables here
+
+//fetch weather API
+//addEventListener to search button
+//hide intro paragraph
+//display weather for city
+
+//fetch cocktail API here?
+//addEventListener to search button
+//hide middle paragraph
+//if (user input = bourbon){
+    //displayBourbonDrinks();
+//};
+//else if (user input = rum){
+    //displayRumDrinks();
+//};
+//else (){
+    //displayVodkaDrinks();
+//};
+
+//should all of these be a fetch function with a specified path to their respective ingredients?
+//function displayBourbonDrinks(){
+    //
+//};
+//function displayBourbonDrinks(){
+    //
+//};
+//function displayBourbonDrinks(){
+    //
+//};
+
+//add localstorage.setItem("previous search", user input);
+//(previously delared variable to call where to dispaly on page = localstorage.getItem("previous search")
